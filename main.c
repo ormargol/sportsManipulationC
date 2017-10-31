5c946b1688cea3731169be58218d33be0a8af4ab
Orr Margaliot <ormargol@gmail.com>
2017-10-31 21:43:02 +0200

    init api
    Signed-off-by: Orr Margaliot <ormargol@gmail.com>

--- main.c
+++ main.c
@@ -1,41 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define GROUPS_NUM 8
+#define TEAMS_NUM 8
+#define GROUP_SIZE 4
+#define GROUPS_NUM (TEAMS_NUM / GROUP_SIZE)
+#define true 1
+#define false 0
 
 typedef int bool;
 
-struct team_t;
-typedef struct team_t team;
-
-struct {
+typedef struct {
     int id;
     int score;
-    team* prev;
-    team* next;
-    team* parent;
-    team* right;
-    team* left;
-}team_t;
+}team;
 
 typedef struct {
     int id;
-    team* teams;
+    team* teams[GROUP_SIZE];
 }group;
 
 typedef struct {
-    team* cup_root;
-    group* groups_head;
-    team* teams_head;
-    bool strength_graph[GROUPS_NUM][GROUPS_NUM];
-    bool positive_manipulators[GROUPS_NUM][GROUPS_NUM];
-    bool negative_manipulators[GROUPS_NUM][GROUPS_NUM];
+    group* groups[GROUPS_NUM];
+    team* teams[TEAMS_NUM];
+    bool strength_graph[TEAMS_NUM][TEAMS_NUM];
+    bool positive_manipulators[TEAMS_NUM][TEAMS_NUM];
+    bool negative_manipulators[TEAMS_NUM][TEAMS_NUM];
 }league;
 
-int algorithm_init(league* league) {
+int algorithm_init(league* lg) {
+    int g, t1, t2;
+    for (g = 0; g < GROUPS_NUM; g++) {
+        lg->groups[g] = malloc(sizeof(group));
+        lg->groups[g]->id = g + 1;
+        for (t1 = 0; t1 < GROUP_SIZE; t1++) {
+            lg->groups[g]->teams[t1] = malloc(sizeof(team));
+            lg->groups[g]->teams[t1]->id = g * GROUP_SIZE + t1;
+        }
+    }
+    for (t1 = 0; t1 < TEAMS_NUM; t1++) {
+        for (t2 = 0; t2 < TEAMS_NUM; t2++) {
+            if (t2 >= t1 + 1) {
+                lg->strength_graph[t1][t2] = true;// t1 beats t2
+            } else {
+                lg->strength_graph[t1][t2] = false;
+            }
+            lg->positive_manipulators[t1][t2] = false;
+            lg->negative_manipulators[t1][t2] = false;
+        }
+    }
+    lg->positive_manipulators[3][4] = true;
+    lg->positive_manipulators[6][4] = true;
+    lg->positive_manipulators[8][4] = true;
     return 0;
 }
 
-int algorithm_execute(league* league, team* etam, bool win_manipulates) {
+int algorithm_execute(league* league, team* team, bool win_manipulates) {
     return 0;
 }
 
