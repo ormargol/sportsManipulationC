5eaa7a1d12a2155e3578292ec6ebf4884029f5c5
Orr Margaliot <ormargol@gmail.com>
2017-11-06 13:13:41 +0200

    add test infrastructure and plan
    Signed-off-by: Orr Margaliot <ormargol@gmail.com>

---
+++ test.c
@@ -0,0 +1,10 @@
+//modify app to read data from argv.
+//strength is same but first argument is the chosen team and rest are manipulators for that team.
+//run app with each team as chosen one, and all options to select which teams are manipulators for it.
+//calculate required result using test_plan.txt file.
+//if on the line of chosen team we sent all required manipulators, expect positive result.
+//other ways expect negative result.
+//modify app to be able to run with only result (the way the test will run it).
+//allow app to run with full logs.
+//when test found failure, it runs with full logs for debug purpose.
+
